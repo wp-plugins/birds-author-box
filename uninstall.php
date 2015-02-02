0825d6083887e2c27ea5be901a9946dba708188e
fredserva <fredserva@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-02 07:29:56 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/birds-author-box/tags/1.0.0@1080660 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Fired when the plugin is uninstalled.
+*
+* @package    Birds_Authorbox
+* @since      1.0.0
+*/
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
